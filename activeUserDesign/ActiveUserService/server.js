4b2f13c68c06861b2643040213660bf9b4a21e9e
rkumar <rkumar@agoda.com>
2020-08-09 14:07:45 +0700

    Active user service

---
+++ activeUserDesign/ActiveUserService/server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.status(200).send({ "status": "OK" });
+});
+
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://" + host + ":" + port)
+})
