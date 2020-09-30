2d3be3bb53d7a5f2d9a6b815bfa8d056cd9f9b55
Sunandan Bose <sunandan.riders@gmail.com>
2020-09-30 21:41:10 +0530

    Add cache data test

--- activeUserDesign/ActiveUser/CacheData.test.js
+++ activeUserDesign/ActiveUser/CacheData.test.js
@@ -9,14 +9,12 @@ test('test to see if cacheData is erasing data after 2 secs', async () => {
 	let cacheData = new CacheData(4,Date.now(),2000)
 	await sleep(2000);
 	expect(cacheData.getCachedCount()).toBe(-1)
-	//setTimeout(expect(cacheData.getCachedCount()).toBe(-1), 2000);
 })
 
 test('test to see if cacheData is not erasing data after 1 secs', async () => {
 	let cacheData = new CacheData(4,Date.now(),2000)
 	await sleep(1000);
 	expect(cacheData.getCachedCount()).toBe(4)
-	//setTimeout(expect(cacheData.getCachedCount()).toBe(-1), 2000);
 })
 
 function sleep(ms) {   return new Promise(resolve => setTimeout(resolve, ms)); }
